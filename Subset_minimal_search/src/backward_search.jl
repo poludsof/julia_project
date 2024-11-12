658a2899c80f7fe98787a527c205519edb30a1d4
Sofia Poludina <poludsof@fel.cvut.cz>
2024-11-12 16:04:38 +0100

    simple foeward search

--- Subset_minimal_search/src/backward_search.jl
+++ Subset_minimal_search/src/backward_search.jl
@@ -67,55 +67,162 @@ function dfs_cache(nn, fix_inputs::Vector{Int}, input, output, steps::Int, max_s
     return best_set
 end
 
-function dfs_cache_non_recursive(nn, given_input_set::SBitSet{N,T}, input, output, max_steps::Int) where {N, T}
+function dfs_cache_non_recursive(nn::Chain, given_input_set::SBitSet{N,T}, input::AbstractVector{<:Integer}, output, max_steps::Int) where {N, T}
     visited_local = Set{SBitSet{N,T}}()
     stack = [(given_input_set, 0)]  # all (current subset, depth)
     best_set = given_input_set
 
     while !isempty(stack)
         @timeit to "pop!" current_set, steps = pop!(stack)
-        # current_set, steps = pop!(stack)
 
         if steps >= max_steps || in(current_set, visited_local)
+            if steps >= max_steps
+                println("Max steps reached")
+            end
             continue
         end
 
         push!(visited_local, current_set) # mark the current subset as visited
 
+        # samplovani
+        if length(current_set) <= 760
+            @timeit to "random" adv_founded = check_random_sets(nn, input, output, current_set)
+            println("Random search result: ", adv_founded)
+            if adv_founded
+                # println("Random set FOUND")
+                continue
+            end
+            # if length(current_set) <= 750 && adv_founded == false
+            #     println("returning current set due to adv_founded == false")
+            #     return current_set
+            # end
+        end
+
+        if length(current_set) <= 710  # too long
+            break
+        end
+
         @timeit to "milp" status, _ = adversarial(nn, input, output, current_set)
+        # status = "fff"
         println("TEST ON:", length(current_set), " status: ", status)
 
         if status == :success
             println("stop searching this branch")
             continue
-        elseif length(current_set) < length(best_set)
+        end
+            
+        if length(current_set) < length(best_set)
             best_set = current_set
         end
 
-        # if length(current_set) <= 760  # too long
-        #     break
-        # end
-
+        indx = 0
         for i in current_set
             next_set = current_set ~ SBitSet{N,T}(i)
             if length(next_set) <= 0 || in(next_set, stack) || in(next_set, visited_local)
                 continue
             end
-            
+            indx += 1
+            if indx > 1
+                break
+            end
             @timeit to "push!" push!(stack, (next_set, steps + 1))
         end
     end
-
     return best_set
 end
 
-function minimal_set_dfs(nn::Chain, input, output)
+function test_new_dfs(nn, given_input_set::SBitSet{N,T}, input, output) where {N, T}
+    for i in given_input_set
+        println("i: ", i)
+        if length(given_input_set) <= 700
+            break
+        end
+        
+        status = check_random_sets(nn, input, output, given_input_set)
+        # if status == false
+        #     status, _ = adversarial(nn, input, output, given_input_set)
+        # end
+
+        println("length: ", length(given_input_set), " status: ", status)
+        if status == false
+            given_input_set = given_input_set ~ SBitSet{N,T}(i)    
+            println("Deleted: ", i)    
+        end
+    end
+    return given_input_set
+end
+
+
+function find_minimal_subset_forward(nn::Chain, given_input_set::SBitSet{N,T}, image::AbstractVector{<:Integer}, target_digit::Int) where {N, T}
+    subset = SBitSet{N, T}()
+    candidate_subset = SBitSet{N, T}()
+
+    for i in given_input_set
+        candidate_subset = candidate_subset ∪ SBitSet{N, T}(i)
+
+        status = check_random_sets(nn, image, target_digit, candidate_subset)
+        if status == false
+            return candidate_subset
+        end
+
+        println("Candidate subset: ", length(candidate_subset), " status: ", status)
+    end
+    
+    return subset
+end
+
+function random_img(input::AbstractVector{<:Integer}, current_set::SBitSet{N,T}) where {N, T}
+    random_img = rand(0:1, length(input))
+    random_img = [idx in current_set ? input[idx] : random_img[idx] for idx in 1:length(input)]
+    return random_img
+end
+
+
+function generate_unique_random_sets(input::AbstractVector{<:Integer}, current_set::SBitSet{N,T}, num_sets::Int) where {N, T}
+    unique_sets = Set{Vector{Int}}()
+    
+    while length(unique_sets) < num_sets
+        new_set = random_img(input, current_set)
+        push!(unique_sets, new_set)
+    end
+    
+    return collect(unique_sets)
+end
+
+
+function check_random_sets(nn::Chain, input::AbstractVector{<:Integer}, output, current_set::SBitSet{N,T}) where {N, T}
+    number_sets = 2 ^ (length(input) - length(current_set))
+    number_sets = (number_sets > 1000 || number_sets == 0) ? 1000 : number_sets
+    println("Number of sets: ", number_sets)
+    unique_sets = generate_unique_random_sets(input, current_set, number_sets)
+    for img in unique_sets
+        # println("Random set: ", img)
+        result = argmax(nn(img)) - 1
+        # println("Result: ", result)
+        if result != output
+            # println("Random set found adversarial example")
+            return true
+        end
+    end
+    # array = ones(Int, length(input))
+    # array = [idx in current_set ? input[idx] : array[idx] for idx in 1:length(input)]
+    # if (argmax(nn(array)) - 1) != output
+    #     return true
+    # end
+    # println("Random set not found adversarial example")
+    return false
+end
+
+
+function minimal_set_dfs(nn::Chain, input::AbstractVector{<:Integer}, output)
     given_input_set = collect(1:length(input))
     tmp_inputs = collect(1:4)
 
     input_set = SBitSet{32, UInt32}(given_input_set)
     # result = dfs_cache(nn, given_input_set, input, output, 0, 100, Ref(false))
-    result = dfs_cache_non_recursive(nn, input_set, input, output, 100)
+    # result = dfs_cache_non_recursive(nn, input_set, input, output, 100)
     # dfs(nn, given_input_set, 1, input, output)
+    # result = test_new_dfs(nn, input_set, input, output)
+    result = find_minimal_subset_forward(nn, input_set, input, output)
     return result
 end
