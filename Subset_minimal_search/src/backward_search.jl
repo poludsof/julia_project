252440676842deb4254653ab9f6af02d8ff30184
poludsof <s.poludina19@gmail.com>
2025-04-07 03:48:34 +0200

    Started changing backward_search api

--- Subset_minimal_search/src/backward_search.jl
+++ Subset_minimal_search/src/backward_search.jl
@@ -62,6 +62,54 @@ function expand_backward(sm::Subset_minimal, calc_func::Function, data_model, op
 end
 
 
+function backward_search(sm::Subset_minimal, ii::TT, isvalid::Function, heuristic_fun; time_limit=Inf, terminate_on_first_solution=true) where {TT}
+    println("II", ii)
+    println("II length: ", solution_length(ii))
+    initial_heuristic, full_error = heuristic_fun(ii)
+    println("Initial error: ", full_error, " Initial heuristic: ", initial_heuristic)
+
+    stack = [(initial_heuristic, full_error, ii)]
+    closed_list = Set{TT}()
+
+    best_subsets = ii
+    best_total_len = solution_length(ii)
+
+    steps = 0
+    start_time = time()
+
+    @timeit to "backward(length) search" while !isempty(stack)
+
+        steps += 1
+
+        if time() - start_time > time_limit
+            println("Timeout exceeded, returning last found solution")
+            return best_subsets
+        end
+
+        sort!(stack, by = x -> -x[1])
+        current_heuristic, current_error, ii = pop!(stack)
+        closed_list = push!(closed_list, ii)
+
+        v = @timeit to "isvalid" isvalid(ii)
+
+        if !v
+            println("Valid subset: $(solution_length(ii)) with error: ", current_error)
+            terminate_on_first_solution && return(ii)
+        end
+
+        println("step: $steps, length $(solution_length(ii)) Expanding state with error: $current_error, heuristic: $current_heuristic")
+
+        if solution_length(ii) < best_total_len
+            best_subsets = ii
+            best_total_len = total_len
+        end
+
+        stack = @timeit to "expand_bcwd" expand_bcwd(sm, stack, closed_list, ii, heuristic_fun)
+    end
+    println("Stack is empty")
+    return best_subsets
+end
+
 function backward_search_length_priority(sm::Subset_minimal, (I3, I2, I1), calc_func::Function, calc_func_partial::Function; data_model=nothing, threshold=0.1, max_steps=1000, num_samples=100, time_limit=Inf)    
     confidence = 1 - threshold
     initial_total_err = max_error(sm, calc_func, calc_func_partial, (I3, I2, I1), confidence, data_model, num_samples)
@@ -168,32 +216,31 @@ function backward_search_error_priority(sm::Subset_minimal, (I3, I2, I1), calc_f
     return best_subsets
 end
 
-function expand_bcwd(sm::Subset_minimal, calc_func::Function, calc_func_partial::Function,data_model,  stack, closed_list, (I3, I2, I1), initial_total_err, confidence, num_samples)
+solution_length(ii::Tuple) = solution_length.(ii)
+solution_length(ii::SBitSet) = length(ii)
+solution_length(::Nothing) = 0
+
+new_subsets(ii::SBitSet, idim) = [pop(ii, i) for i in 1:idim if i in ii]
+
+function new_subsets((I3, I2, I1)::T, idims::Tuple) where {T<:Tuple}
+    new_subsets = T[]
     if I3 !== nothing
-        for i in collect(I3)
-            new_subsets = (pop(I3, i), I2, I1)
-            new_error = max_error(sm, calc_func, calc_func_partial, new_subsets, confidence, data_model, num_samples)
-            if isvalid(calc_func, new_error, initial_total_err) && new_subsets ∉ closed_list
-                push!(stack, (new_error, new_subsets))
-            end
-        end
+        append!(new_subsets, [(pop(I3, i), I2, I1) for i in 1:idims[1] if i in I3])
     end
     if I2 !== nothing
-        for i in collect(I2)
-            new_subsets = (I3, pop(I2, i), I1)
-            new_error = max_error(sm, calc_func, calc_func_partial, new_subsets, confidence, data_model, num_samples)
-            if isvalid(calc_func, new_error, initial_total_err) && new_subsets ∉ closed_list
-                push!(stack, (new_error, new_subsets))
-            end
-        end
+        append!(new_subsets, [(I3, pop(I2, i), I1) for i in 1:idims[2] if i in I2])
     end
-    if  I1 !== nothing
-        for i in collect(I1)
-            new_subsets = (I3, I2, pop(I1, i))
-            new_error = max_error(sm, calc_func, calc_func_partial, new_subsets, confidence, data_model, num_samples)
-            if isvalid(calc_func, new_error, initial_total_err) && new_subsets ∉ closed_list
-                push!(stack, (new_error, new_subsets))
-            end
+    if I1 !== nothing
+        append!(new_subsets, [(I3, I2, pop(I1, i)) for i in 1:idims[3] if i in I1])
+    end
+    new_subsets
+end
+
+function expand_bcwd(sm::Subset_minimal, stack, closed_list, ii, heuristic_fun)
+    for new_subset in new_subsets(ii, sm.dims)
+        if new_subset ∉ closed_list
+            new_heuristic, new_error = @timeit to "heuristic"  heuristic_fun(new_subset)
+            push!(stack, (new_heuristic, new_error, new_subset))    
         end
     end
     stack
